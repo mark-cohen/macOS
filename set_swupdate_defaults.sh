32d48ac03929186a1c2499293d6084d5527f7018
Mark Cohen <mcohen@UMRK-III.local>
2019-12-07 20:23:15 -0800

    added script to set swupdate defaults

---
+++ set_swupdate_defaults.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# auto-install security updates, do not auto-intsall macOS updates
+/usr/bin/defaults write /Library/Preferences/com.apple.commerce.plist AutoUpdate -bool false
+/usr/bin/defaults write /Library/Preferences/com.apple.SoftwareUpdate.plist AutomaticallyInstallMacOSUpdates -bool false
+/usr/bin/defaults write /Library/Preferences/com.apple.SoftwareUpdate.plist AutomaticDownload -bool true
+/usr/bin/defaults write /Library/Preferences/com.apple.SoftwareUpdate.plist AutomaticCheckEnabled -bool true
+/usr/bin/defaults write /Library/Preferences/com.apple.SoftwareUpdate.plist CriticalUpdateInstall -bool true
+/usr/bin/defaults write /Library/Preferences/com.apple.SoftwareUpdate.plist ConfigDataInstall -bool true
+/usr/bin/defaults write /Library/Preferences/com.apple.SoftwareUpdate.plist EvaluateCriticalEvenIfUnchanged -bool true
+
+# ignore (hide) macOS 10.15
+/usr/sbin/softwareupdate --ignore "macOS Catalina"
